06e99c95cc903fab9d0b8ec39d191874c7d88eaf
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-01-26 18:00:28 +0300

    Custom Views

--- doctorApp/CustomViews/TableViewCell/FavoritesCell/FavoritesCell.swift
+++ doctorApp/CustomViews/TableViewCell/FavoritesCell/FavoritesCell.swift
@@ -10,7 +10,19 @@ import UIKit
 class FavoritesCell: UITableViewCell {
     
     static let identifier = String(describing: FavoritesCell.self)
+    @IBOutlet var doctorImage: UIImageView!
+    @IBOutlet var doctorName: UILabel!
+    @IBOutlet var doctorTitle: UILabel!
     
-    // displaying Func 
+    // displaying Func
+    func display(Favorite d: Doctor) {
+        NetworkManager.shared.downloadImage(from: d.image ?? "nil") { [weak self] image in
+            DispatchQueue.main.async {
+                self?.doctorImage.image = image
+            }
+        }
+        doctorName.text = d.name
+        doctorTitle.text = d.title
+    }
     
 }
